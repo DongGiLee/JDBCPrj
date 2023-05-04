2ec12372be09d236e95b94442a266c81f2e630dd
DongDong <37203688+DongGiLee@users.noreply.github.com>
2023-05-04 10:14:28 +0900

    init

---
+++ src/ex1/Program.java
@@ -0,0 +1,13 @@
+package ex1;
+
+import java.sql.Connection;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection con;
+		
+	}
+
+}
